a22d84095f78e42a3ba6f889280fc945b44a5384
koji-estride <79740695+koji-estride@users.noreply.github.com>
2022-06-09 15:16:26 +0900

    Stateを知る(useState)

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import ColorfullMessage from "./components/ColorfullMessage";
 
 const App = () => {
-  const onClickButton = () => alert();
+  const onClickCountUp = () => {
+    setNum(num + 1);
+  };
+  // [stateの変数名, stateを更新する関数名] = useState(初期値);
+  const [num, setNum] = useState(0);
   return (
     <>
       <h1 style={{ color: "red" }}>こんにちは！</h1>
       <ColorfullMessage color="blue">お元気ですか?</ColorfullMessage>
       <ColorfullMessage color="pink">元気です!</ColorfullMessage>
-      <button onClick={onClickButton}>ボタン</button>
+      <button onClick={onClickCountUp}>カウントアップ</button>
+      <p>{num}</p>
     </>
   );
 };
